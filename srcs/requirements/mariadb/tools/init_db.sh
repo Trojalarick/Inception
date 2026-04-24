8851fc57de195061f977b5c7e636e49f0ce3dd31
salhali <salhali@debian.https://salhali.42.fr>
2026-04-24 18:53:52 -0400

    fix object chache php file and fix the require_once ABSPATH . 'wp-settings.php';

--- srcs/requirements/mariadb/tools/init_db.sh
+++ srcs/requirements/mariadb/tools/init_db.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+# Start MariaDB in background
 mysqld_safe &
 
-# Wait until MariaDB is actually ready
+# Wait until ready
 until mysqladmin ping -h "localhost" --silent; do
     sleep 1
 done
 
-# Run SQL setup
-mysql -u root <<EOF
+# ONLY run setup if database doesn't exist
+if [ ! -d "/var/lib/mysql/$MYSQL_DATABASE" ]; then
+    echo "Initializing database..."
 
+    mysql -u root <<EOF
 ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
 
 CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
@@ -19,11 +22,11 @@ CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
 GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
 
 FLUSH PRIVILEGES;
-
 EOF
+fi
 
 # Stop background server
 mysqladmin -u root -p"$MYSQL_ROOT_PASSWORD" shutdown
 
-# Start MariaDB in foreground
+# Start in foreground (important for Docker)
 exec mysqld_safe
--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 
-mkdir -p /var/www/html
 cd /var/www/html
 
-# Download WordPress
-wget https://wordpress.org/trixie.tar.gz
-tar -xzf trixie.tar.gz --strip-components=1
-rm trixie.tar.gz
+# Wait for MariaDB
+until nc -z mariadb 3306; do
+    sleep 1
+done
 
-# Permissions
-chown -R www-data:www-data /var/www/html
-chmod -R 755 /var/www/html
+# Create wp-config if not exists
+if [ ! -f wp-config.php ]; then
+    cp wp-config-sample.php wp-config.php
 
-# Start PHP-FPM
+    sed -i "s/database_name_here/wordpress/" wp-config.php
+    sed -i "s/username_here/salhali/" wp-config.php
+    sed -i "s/password_here/wp_password/" wp-config.php
+    sed -i "s/localhost/mariadb/" wp-config.php
+
+    # ADD REDIS CONFIG (IMPORTANT: BEFORE wp-settings)
+    sed -i "/\/\* That's all, stop editing! Happy publishing. \*\//i \
+define('WP_REDIS_HOST', 'redis');\n\
+define('WP_REDIS_PORT', 6379);\n\
+define('WP_REDIS_CLIENT', 'phpredis');\n" wp-config.php
+fi
+
+# ALWAYS remove old cache file (VERY IMPORTANT)
+rm -f /var/www/html/wp-content/object-cache.php
+
+# Start PHP
 exec php-fpm8.4 -F
